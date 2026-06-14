15e66a0708b1e1a6a10d2fa163cb13591c6b5702
Rudra <grudraksh825@gmail.com>
2026-06-14 08:02:37 +0530

    updated axios

--- vite-project/src/services/noteService.jsx
+++ vite-project/src/services/noteService.jsx
@@ -1,13 +1,14 @@
 import api from "../api/axios"
-export const getNotes=async()=>{
-    const response= await api.get("/notes");
-    return response.data.notes || response.data;
+export const getNotes = async () => {
+  const response = await api.get("/notes")
+  return response.data.notes || response.data;
 }
-export const createNote=async(noteData)=>{
-    const response= await api.post("/notes",noteData);
-    return response.data;
+export const createNote = async (noteData) => {
+  const response = await api.post("/notes", noteData);
+  return response.data;
 }
-export const deleteNote=async(id)=>{
-    const response= await api.delete(`/notes/${id}`);
-    return response.data;
+export const deleteNote = async (id) => {
+  const response = await api.delete(`/notes/${id}`);
+  //                               ↑ Template literal — dynamic URL
+  return response.data;
 }
